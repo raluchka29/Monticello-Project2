7f8342f908f7074da81142f5e980c5ff76ac1337
Raluca <raluca.elena.butacu@gmail.com>
2023-11-28 13:10:35 +0200

    second commit

--- monticello.js
+++ monticello.js
@@ -1,5 +1,16 @@
 $(function() {
   AOS.init();
 });
+document.getElementById("myForm").submit();
+
+AOS.init();
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var welcomeSection = document.querySelector('.welcome');
+    if (welcomeSection) {
+        welcomeSection.setAttribute('data-aos', 'fade-right');
+    }
+});
 
 
